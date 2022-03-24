d5155fd914cf40e2a97c05c4d32b8a992d4341f1
Alexandra <shikunova@Alexandras-MacBook-Air.local>
2022-03-24 10:11:19 +0300

    initial commit

---
+++ static/test_data.py
@@ -0,0 +1,47 @@
+results = {
+    'North America':{
+        'mammals':125530,
+        'bats':100460,
+        'birds':16300,
+        'insects':252456
+    },
+    'Europe':{
+        'mammals':129470,
+        'bats':123690,
+        'birds':5970,
+        'insects':1000069
+    },
+    'South':{
+        'mammals':110930,
+        'bats':121310,
+        'birds':7060,
+        'insects':263363
+    },
+}
+
+result_print = {
+    125530: {'result':'Indiana bat',
+            'text':'A little bat native to the US! Are you an American citizen?'},
+    100460:{'result':'Pipistrelle',
+            'text':'A cute tiny bat! Also, your name is Italian but you populate vast areas of Europe and North America.'},
+    16300:{'result':'Sprague\'s Pipit',
+            'text':'What a cute name! Carries the name of a botanical artist Isaac Sprauge. Beautiful singing voice as well!'},
+    252456:{'result':'Monarch butterfly',
+            'text':'A gorgeous butterfly, what else to say?'},
+    129470:{'result':'Human',
+            'text':'Yes, Motus tracks humans as well. They\'re watching you (in Europe and America).'},
+    123690:{'result':'Serotine bat',
+            'text':'You misread it as \'serotonin bat\', didn\'t you? That is because you bring happiness everywhere you go!'},
+    5970:{'result':'Rock pigeon',
+            'text':'The legendary city bird. Have you considered feeding your local pigeons? Hurry to befriend your local pigeon gang.'},
+    1000069:{'result':'Buff-tailed bumblebee',
+            'text':'Flying around, helping the flowers bloom. БЖЖЖЖЖЖЖЖЖЖЖЖЖЖЖЖЖ!'},
+    110930:{'result':'Northern quoll',
+            'text':'What an adorable carnivorous cat! An Australian native too.'},
+    121310:{'result':'Orange horseshoe bat',
+            'text':'Look at your bright orange fur! A proud citizen of Australia, send my regards to your kangaroo friends.'},
+    7060:{'result':'Yellow-billed cuckoo',
+            'text':'Americans call you rain crow or storm crow. But you\'re not a crow... Coo-ckoo, coo-ckoo!'},
+    263363:{'result':'Hymenopteran',
+            'text':'A bee, a wasp or something else? Stinging others or pollinating flowers? You tell me!'},
+}
